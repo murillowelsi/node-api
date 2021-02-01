dc4b4fc2bb352b27a54983d63120216f6358905b
Murillo Welsi <murillo.welsi@gmail.com>
2021-02-01 10:53:44 +0000

    add files

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://murillo:ZRJuIdp5FgFNAhvP@ndstr.tot9b.mongodb.net/ndstr?retryWrites=true&w=majority',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodestoremurillo;AccountKey=ctOh4PtU5sx+ATn5opfOj9xkENUhzICrHl9e0AFVwU/hiyW5VyzG3bT15ekzx9bAt91ShasWLcViaJosVW7ybw==;EndpointSuffix=core.windows.net'
+}
